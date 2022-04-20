02cb7d976d3c5b4f76a59fc08c6dfba0e2e84779
Nikita <grishchenkonikita@mail.ru>
2022-04-20 20:25:06 +0300

    changed detail survey page

--- survey/views.py
+++ survey/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic import ListView
 from django.views.generic.detail import DetailView
 
-from .models import Survey
+from .models import QuestionForSurvey, Survey, UserSurvey
 
 
 class SurveyListView(ListView):
@@ -19,7 +19,13 @@ class SurveyDetailView(DetailView):
     model = Survey
     template_name = "survey/detail.html"
 
-    # def get_context_data(self, **kwargs):
-    #     context = super().get_context_data(**kwargs)
-    #     context['object_list'] = Survey.objects.all().order_by('-id')
-    #     return context
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        survey = Survey.objects.get(id=self.kwargs['pk'])
+        context['survey'] = survey
+        context['questions'] = QuestionForSurvey.objects.filter(survey=survey)
+        context['user_survey'] = UserSurvey.objects.filter(survey=survey, user=self.request.user)
+        return context
+
